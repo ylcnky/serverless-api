0744518a0dea2e4cbb3a934ce5d27fe22c041f63
ylcnky <mehmet.yalcinkaya@aalto.fi>
2017-08-25 17:13:44 +0300

    API Gateway POST method with func

--- handler.js
+++ handler.js
@@ -1,6 +1,27 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+
+  if(event.queryStringParameters && event.queryStringParameters.name){
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'A test for the ' + event.queryStringParameters.name+ ' parameter'
+      }),
+    };)
+  }
+
+  if(event.httpMethod === "POST" && event.body){
+    let json = JSON.parse(event.body)
+
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Hey! I have received a JSON object',
+        object: json
+      }),
+    };)
+  }
   const response = {
     statusCode: 200,
     body: JSON.stringify({
